c1c5f69a5621b3a4bc4b2df22ad288547036071c
Digvijay Barman <70092222+DigvijayBarman@users.noreply.github.com>
2022-08-23 08:40:06 +0530

    Add files via upload

---
+++ new classroom.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+int num;				 //Number of students in the class
+
+student[i].RollNo;		//Declaring array of struct Student
+student[i].Name;
+student[i].Course;
+
+struct student{
+	char Name[50];
+	int RollNo;
+	char Course[50];
+	
+};
+
+int ByName(char x[50]){
+	char name[50];
+	int j;
+	
+	for(j=0;j<num;j++){
+		name=student.Name
+		
+	}
+	
+}
+
+int main(){
+	
+	
+	
+	printf("Enter the number of students in the class: ");
+	scanf("%d",&num);
+	
+
+	
+	Printf("\nNumber of students in class: %d ",num);
+	
+	
+	for (i=0;i<num;i++){
+		
+		printf("Enter the Roll Number of student %d: ",i+1);		//Input Student Roll Number
+   		scanf(" %d", &student[i].RollNo);
+   		
+   		printf("\nEnter the Name of student %d: ",i+1);				//Input Student Name
+   		scanf(" %s", &student[i].Name);
+   		
+   		printf("\nEnter the Course of student %d: ",i+1);			//Input Student Course
+   		scanf(" %s", &student[i].Course);
+   		
+   		
+   		
+   		
+	}
+	return 0;
+	
+}
